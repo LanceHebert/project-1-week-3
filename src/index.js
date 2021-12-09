4cfc3d84019f144985ff5c71731673f021651036
Lance Hebert <LSUHEBERT@gmail.com>
2021-12-09 09:43:27 -0800

    Added div and empty div elements to modularize search results.

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@
 
 //**************************************************** */
 //adds event listener, sends ticker to fetch request.
+
 coinInput();
 
 function coinInput() {
@@ -35,7 +36,7 @@ function coinInput() {
 // replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
 function getAPI(coinValue, dropValue) {
   // var request = require("request");
-  var url = `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${coinValue}&to_currency=USD&apikey=B3BGRQPX1AOZLTLP`;
+  var url = `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${coinValue}&to_currency=USD&apikey=6MHEHPP7MI5FPCOG`;
   // request.get(
   fetch(url, {
     url: url,
@@ -45,56 +46,67 @@ function getAPI(coinValue, dropValue) {
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
-      getWeeklyValue(coinValue, dropValue);
       renderCoinObj(data);
+      getWeeklyValue(coinValue, dropValue);
     })
     .catch((err) => {
       console.log(err.message);
-      alert("API Current has reached max use.");
-      return;
+      console.log("API Current has an Error.");
     });
 }
 
 //show the coin chosen
+
 const coinDiv = document.createElement("div");
+
 function renderCoinObj(coin) {
-  const p = document.createElement("p");
+  const firstCoinDiv = document.createElement("div");
   const parentCoinDiv = document.querySelector("#coins");
+  const p = document.createElement("p");
   const removeBtn = document.createElement("button");
   const increaseBtn = document.createElement("button");
-  const spanCurrentUsd = document.createElement("span");
+  const spanCurrent = document.createElement("span");
+  const emptyDiv = document.createElement("div");
 
+  p.classList.add("pClass");
   let currentUsd = [];
   currentUsd.push(
     parseInt(coin["Realtime Currency Exchange Rate"]["5. Exchange Rate"], 10)
   );
+  // let counter = 1;
+  emptyDiv.classList.add("emptyDiv");
   const ticker =
     coin["Realtime Currency Exchange Rate"]["1. From_Currency Code"];
   // console.log(counter);
 
   increaseBtn.innerHTML = `<i class="fas fa-arrow-circle-up">Increase Coin</i>
-  `;
+   `;
   increaseBtn.style.color = "yellow";
   removeBtn.innerText = "Delete";
   removeBtn.style.fontSize = "10px";
-  spanCurrentUsd.textContent = currentUsd[currentUsd.length - 1];
-  spanCurrentUsd.style.color = "#E3B04B";
 
   //Need this to update the original amount somehow
   increaseBtn.addEventListener("click", (event) => {
     for (let element of currentUsd) {
       element += currentUsd[currentUsd.length - 1];
       currentUsd.push(element);
-      // console.log("this is last value:", currentUsd[currentUsd.length - 1].value);
-      // console.log("this is array:", currentUsd);
-      p.textContent = `${ticker}  $ ${spanCurrentUsd}`;
+      console.log("this is last value:", currentUsd[currentUsd.length - 1]);
+      console.log("this is array:", currentUsd);
+      p.innerHTML = `${ticker}  $ <span>${
+        currentUsd[currentUsd.length - 1]
+      }<br></span>`;
       return;
     }
   });
-  p.append(spanCurrentUsd);
-  p.textContent = `${ticker} $`;
+  spanCurrent.textContent = `${currentUsd[currentUsd.length - 1]}`;
+
+  p.innerHTML = `${ticker}  $ <span>${
+    currentUsd[currentUsd.length - 1]
+  }<br></span>`;
+
   // p.textContent = ticker + "  $" + currentUsd[currentUsd.length - 1];
-  coinDiv.append(increaseBtn, p, spanCurrentUsd, removeBtn);
+  firstCoinDiv.append(increaseBtn, p, emptyDiv, removeBtn);
+  coinDiv.append(firstCoinDiv);
   parentCoinDiv.append(coinDiv);
 
   //remove the coin
@@ -102,10 +114,56 @@ function renderCoinObj(coin) {
     event.target.parentNode.remove();
   });
 }
+// function renderCoinObj(coin) {
+//   const firstCoinDiv = document.createElement('div')
+//   const parentCoinDiv = document.querySelector("#coins");
+//   const p = document.createElement("p");
+//   const removeBtn = document.createElement("button");
+//   const increaseBtn = document.createElement("button");
+
+//   let currentUsd = [];
+//   currentUsd.push(
+//     parseInt(coin["Realtime Currency Exchange Rate"]["5. Exchange Rate"], 10)
+//   );
+//   const ticker =
+//     coin["Realtime Currency Exchange Rate"]["1. From_Currency Code"];
+//   // console.log(counter);
+
+//   increaseBtn.innerHTML = `<i class="fas fa-arrow-circle-up">Increase Coin</i>
+//   `;
+//   increaseBtn.style.color = "yellow";
+//   removeBtn.innerText = "Delete";
+//   removeBtn.style.fontSize = "10px";
+//   spanCurrentUsd.textContent = currentUsd[currentUsd.length - 1];
+//   spanCurrentUsd.style.color = "#E3B04B";
+
+//   //Need this to update the original amount somehow
+//   increaseBtn.addEventListener("click", (event) => {
+//     for (let element of currentUsd) {
+//       element += currentUsd[currentUsd.length - 1];
+//       currentUsd.push(element);
+//       // console.log("this is last value:", currentUsd[currentUsd.length - 1].value);
+//       // console.log("this is array:", currentUsd);
+//       p.textContent = `${ticker}  $ ${spanCurrentUsd}`;
+//       return;
+//     }
+//   });
+//   p.append(spanCurrentUsd);
+//   p.textContent = `${ticker} $`;
+//   // p.textContent = ticker + "  $" + currentUsd[currentUsd.length - 1];
+//   firstCoinDiv.append(increaseBtn, p, removeBtn);
+//   coinDiv.append(firstCoinDiv);
+//   parentCoinDiv.append(coinDiv);
+
+//   //remove the coin
+//   removeBtn.addEventListener("click", (event) => {
+//     event.target.parentNode.remove();
+//   });
+// }
 
 function getWeeklyValue(coinValue, dropValue) {
   // var request = require("request");
-  var url = `https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_WEEKLY&symbol=${coinValue}&market=USD&apikey=B3BGRQPX1AOZLTLP`;
+  var url = `https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_WEEKLY&symbol=${coinValue}&market=USD&apikey=6MHEHPP7MI5FPCOG`;
   //https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_WEEKLY&symbol=BTC&market=CNY&apikey=demo
   // request.get(
   fetch(url, {
@@ -120,14 +178,14 @@ function getWeeklyValue(coinValue, dropValue) {
     })
     .catch((err) => {
       console.log(err.message);
-      alert("API Weekly has reached max use.");
-      return;
+      console.log("API Weekly has an Error");
     });
 }
 
 function renderWeeklyObj(coinWeekly, dropValue) {
   const span = document.createElement("span");
-  const divP = coinDiv.querySelectorAll("p");
+  const divP = coinDiv.querySelector("p");
+  const divWeekly = document.querySelectorAll(".emptyDiv");
 
   const date = new Date();
   dateFull =
@@ -149,40 +207,49 @@ function renderWeeklyObj(coinWeekly, dropValue) {
         "4a. close (USD)"
       ]
     );
-   
-      span.textContent = currentWeeklyUsd[0];
-      divP[divP.length - 1].append(`   Week of ${week1}:   $`, span);
-    
+
+    span.textContent = currentWeeklyUsd[0];
+    divWeekly[divWeekly.length - 1].append(
+      `   Week ending ${week1}:   $`,
+      span
+    );
+    // divP.append(pWeekly);
   } else if (dropValue === "2 weeks ago") {
     currentWeeklyUsd.push(
       coinWeekly["Time Series (Digital Currency Weekly)"][`${week2}`][
         "4a. close (USD)"
       ]
     );
-    
-      span.textContent = currentWeeklyUsd[0];
-      divP[divP.length - 1].append(`   Week of ${week2}:   $`, span);
-    
+
+    span.textContent = currentWeeklyUsd[0];
+    divWeekly[divWeekly.length - 1].append(
+      `   Week ending ${week2}:   $`,
+      span
+    );
   } else if (dropValue === "3 weeks ago") {
     currentWeeklyUsd.push(
       coinWeekly["Time Series (Digital Currency Weekly)"][`${week3}`][
         "4a. close (USD)"
       ]
     );
-    
-      span.textContent = currentWeeklyUsd[0];
-      divP[divP.length - 1].append(`   Week of ${week3}:   $`, span);
-   
+
+    span.textContent = currentWeeklyUsd[0];
+    divWeekly[divWeekly.length - 1].append(
+      `   Week ending ${week3}:   $`,
+      span
+    );
   } else if (dropValue === "4 weeks ago") {
     currentWeeklyUsd.push(
       coinWeekly["Time Series (Digital Currency Weekly)"][`${week4}`][
         "4a. close (USD)"
       ]
     );
-   
-      span.textContent = currentWeeklyUsd[0];
-      divP[divP.length - 1].append(`   Week of ${week4}:    $`, span);
-    
+
+    span.textContent = currentWeeklyUsd[0];
+    divWeekly[divWeekly.length - 1].append(
+      `   Week ending ${week4}:    $`,
+      span
+    );
   } else {
     console.log("No week Error");
   }
